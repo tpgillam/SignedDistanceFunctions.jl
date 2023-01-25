4be5a7c75a3b5f93a4b8e7f487c4f14ed42f03cd
Documenter.jl <documenter@juliadocs.github.io>
2023-01-25 21:12:30 +0000

    build based on c73c5e6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SignedDistanceFunctions","category":"page"},{"location":"#SignedDistanceFunctions","page":"Home","title":"SignedDistanceFunctions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SignedDistanceFunctions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SignedDistanceFunctions]","category":"page"}]
+}
